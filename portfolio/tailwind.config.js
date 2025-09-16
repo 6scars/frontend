ca1335e133ed7a7d83d38c5cdd8b57334ecfaccd
6scars <zasalatol@o2.pl>
2025-09-17 00:42:24 +0200

    add some media queries

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   safelist: ["contact"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      
+    },
+    screens: {
+      'md-832':'832px',
+      'max-md-832': {max: '831px'}
+    }
+  },
   plugins: [],
 }
