5897a64112bb610289a2fb0555d61ee1c0edafff
6scars <zasalatol@o2.pl>
2025-09-22 23:48:04 +0200

    add content and go sleep

--- portfolio/projects/mini-spotify/src/Play/Play.jsx
+++ portfolio/projects/mini-spotify/src/Play/Play.jsx
@@ -1,4 +1,4 @@
-import './Play.css'
+import "./Play.css";
 export default function Play() {
   return (
     <div
@@ -6,15 +6,36 @@ export default function Play() {
       flex justify-between
     "
     >
-      <div className="play-left-section bg-white h-full w-[300px] min-w-[300px] max-w-[300px] flex">
-        <div className="song-image-container"></div>
-        <img alt="song-image" src="/mini-spotify/images/songPictures/cat1.jpg" className="song__image w-5 h-5"/>
-        <div>
-          <p>LIFETIMES</p>
-          <p>Katy Parry</p>
+      <div className="play-left-section h-full w-[300px] min-w-[300px] max-w-[300px] flex">
+        <div className="song-image-container w-[70px] h-full border-1
+          flex
+        ">
+          <img
+            alt="song-image"
+            src="/mini-spotify/images/songPictures/cat1.jpg"
+            className=" h-full w-full object-cover rounded-md"
+          />
+        </div>
+
+        <div className="title-authors border-1 flex-1 
+          flex flex-col
+        ">
+          <a className="title text-white cursor-pointer hover:underline">LIFETIMES</a>
+          <a className="authors text-[var(--help-color)] cursor-pointer hover:underline ">Katy Parry</a>
         </div>
       </div>
-      <div className="play-center-section bg-blue-500 h-full w-[400px] min-w-[200px] max-w-[600px]"></div>
+      <div className="play-center-section bg-blue-500 h-full w-[400px] min-w-[200px] max-w-[600px]
+        flex flex-col items-center
+      ">
+        <div className="w-full flex-1 flex justify-center items-center gap-5">
+        <a><img alt="start-music" src="/mini-spotify/images/logos/previousSong.svg" className="w-10 h-full cursor-pointer" /></a>
+        <a><img alt="start-music" src="/mini-spotify/images/logos/startSong.svg" className="w-10 h-full cursor-pointer" /></a>
+        <a><img alt="start-music" src="/mini-spotify/images/logos/nextSong.svg" className="w-10 h-full cursor-pointer" /></a>
+        </div>
+        <div className="w-full flex-1 flex justify-center items-center">
+            <input className="bg-white h-10 w-full" placeholder="input"/>
+        </div>
+      </div>
       <div className="play-right-section bg-amber-500 h-full w-[300px] min-[150px]"></div>
     </div>
   );
